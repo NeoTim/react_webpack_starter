6f4d5d1fb2b3b33a757eba864b6eee1d01ad622b
damoqiongqiu <damoqiongqiu@126.com>
2018-09-05 17:19:02 +0800

    using font file inside SCSS

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ class App extends Component {
         return (
             <div>
                 <h1>My React App</h1>
+                <button type="button" class="btn btn-default" aria-label="Left Align">
+                    <span class="glyphicon glyphicon-align-left" aria-hidden="true" />
+                </button>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,15 @@ module.exports = {
                     },
                     "sass-loader"
                 ]
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "fonts/[name].[ext]"
+                    }
+                }
             }
         ]
     },
