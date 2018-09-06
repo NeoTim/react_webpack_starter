2cdbec57a5d20176d378fb549223c5e1df8c2b21
damoqiongqiu <damoqiongqiu@126.com>
2018-09-06 11:20:54 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -69,5 +69,17 @@ module.exports = {
     ],
     devServer: {
         historyApiFallback: true
+    },
+    optimization: {
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        }
     }
 };
